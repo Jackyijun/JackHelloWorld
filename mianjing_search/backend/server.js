0fbcd96ab7138c366935c311340477fa6cc758f7
Jack Yijun <atlantisjackl@gmail.com>
2024-06-17 00:57:06 -0700

    inclusive search, no jwt

--- mianjing_search/backend/server.js
+++ mianjing_search/backend/server.js
@@ -3,16 +3,51 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-app.get('/get-company-info', (req, res) => {
-    const companyName = req.query.company;
-    const filePath = path.join(__dirname, 'pdfs', `${companyName}.pdf`);
+app.use(express.json());
 
-    fs.readFile(filePath, (err, data) => {
+app.get('/get-company-info', (req, res) => {
+    const companyName = req.query.company.toLowerCase();
+    const directoryPath = path.join(__dirname, 'pdfs');
+
+    console.log(`Scanning directory: ${directoryPath}`);
+
+    fs.access(directoryPath, fs.constants.R_OK, (err) => {
         if (err) {
-            return res.status(404).send('PDF not found');
+            console.error('No read access to directory:', err);
+            return res.status(500).send('No read access to directory');
         }
-        const base64Data = data.toString('base64');
-        res.json({ pdf: base64Data });
+
+        fs.readdir(directoryPath, (err, files) => {
+            if (err) {
+                console.error('Unable to scan directory:', err);
+                return res.status(500).send('Unable to scan directory');
+            }
+
+            // Filter files that contain the company name in their file name
+            const matchedFiles = files.filter(file => file.toLowerCase().includes(companyName));
+
+            if (matchedFiles.length === 0) {
+                return res.status(404).send('No matching files found');
+            }
+
+            // Read and encode each matched file as base64
+            const fileDataPromises = matchedFiles.map(file => {
+                return new Promise((resolve, reject) => {
+                    const filePath = path.join(directoryPath, file);
+                    fs.readFile(filePath, (err, data) => {
+                        if (err) {
+                            return reject(err);
+                        }
+                        const base64Data = data.toString('base64');
+                        resolve({ fileName: file, data: base64Data });
+                    });
+                });
+            });
+
+            Promise.all(fileDataPromises)
+                .then(results => res.json(results))
+                .catch(err => res.status(500).send('Error reading files'));
+        });
     });
 });
 
